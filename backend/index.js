fcc7a1a521041aeab0f33f74577bc06530448958
pichuhat <leo@adida.net>
2026-06-03 01:14:29 +0000

    test d

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(express.json());
 app.use(cookieParser())
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(5000, () => {
     console.log(`✅ Minecraft Price API is running on port ${PORT}`);
 });
