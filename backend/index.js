b5f10c702fb29430cc31e730c40003dded40c070
pichuhat <leo@adida.net>
2026-06-03 01:17:04 +0000

    test f (guh)

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,10 @@ const path = require("path")
 
 app.use("/static", express.static(path.join(__dirname, "static")));
 
+app.get('/', (req, res) => {
+    res.send("hello")
+})
 
 app.listen(5000, () => {
-    console.log(`✅ Minecraft Price API is running on port ${PORT}`);
+    console.log(`✅ Minecraft Price API is running on port 5000`);
 });
