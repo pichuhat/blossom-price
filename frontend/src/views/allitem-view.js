d59bd8acb4e89a4ca53b16385a3727e721288708
pichuhat <leo@adida.net>
2026-06-05 20:38:15 +0000

    page counter visual

--- frontend/src/views/allitem-view.js
+++ frontend/src/views/allitem-view.js
@@ -3,16 +3,21 @@ import { LitElement, html, css } from 'https://esm.sh/lit@3';
 export class AllItemView extends LitElement {
   static properties = {
     items: {type: Object},
-    loading: {type: Boolean}
+    loading: {type: Boolean},
+    page: {type: Number},
+    maxPages: {type: Number}
   }
 
   constructor() {
     super()
     this.loading = true
+    this.page = 1
+    this.maxPages = 1
   }
 
   connectedCallback() {
     super.connectedCallback()
+    this._getMaxPages()
     this._fetchItems()
   }
 
@@ -21,9 +26,20 @@ export class AllItemView extends LitElement {
     return value.replace(/\\u([0-9a-fA-F]{4})/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)))
   }
 
+  async _getMaxPages() {
+    try {
+      const response = await fetch("https://blossom-price.onrender.com/api/pagecount")
+
+      if (!response.ok) throw new Error("Server fetch issue")
+      
+      const result = await response.json()
+      this.maxPages = result.count
+    }
+  }
+
   async _fetchItems() {
     try {
-      const response = await fetch("https://blossom-price.onrender.com/api/allitems", {
+      const response = await fetch("https://blossom-price.onrender.com/api/allitems?page=" + this.page, {
         method: "GET",
         credentials: 'include'
       })
@@ -37,6 +53,24 @@ export class AllItemView extends LitElement {
     } finally {
       this.loading = false
     }
+  }
+
+  _nextPage() {
+    this.page++
+  }
+
+  _previousPage() {
+    this.page--
+  }
+
+  _customPage() {
+    const input = window.prompt("Enter page number 1-" + this.maxPages)
+    if (input === null) return;
+    if (/^\d+$/.test(input.trim()) && input > 0 && input <= this.maxPages) {
+    this.page = parseInt(input, 10);
+} else {
+  window.alert("Invalid input")
+}
   }
 
   static styles = css`
@@ -91,10 +125,19 @@ export class AllItemView extends LitElement {
   `;
 
   render() {
-    if (this.loading) return html`Please wait...`
+    if (this.loading) return html`
+    <h1>All Items</h1>
+    <span>Page ${this.page}/${this.maxPages}</span> <button disabled>Previous</button><button disabled>Next</button><button disabled>...</button>
+<br><br>
+    Please wait...
+    `
+
+    const previousDisabled = this.page == 1 ? "disabled" : ""
+    const nextDisabled = this.page == this.maxPages ? "disabled" : ""
 
     return html`
       <h1>All Items</h1>
+      <span>Page ${this.page}/${this.maxPages}</span> <button ${previousDisabled} onclick="this._previousPage()">Previous</button><button ${nextDisabled} onclick="this._nextPage()">Next</button><button onclick="this._customPage()">...</button>
       <div class="grid">
         ${this.items.map(item => html`
           <div class="card">
